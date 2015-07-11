9953e17dd69d49ed2a1922a525329dc206ba1951
seven <471148761@qq.com>
2015-07-11 21:49:55 +0800

    日记 相册 前台js

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -20,7 +20,7 @@ class UserController extends CommonController {
         $upload->maxSize   =     3145728 ;// 设置附件上传大小  
         //$upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型  
  		$upload->rootPath   =     './Public';
-        $upload->savePath  =      './Uploads/Album/'; // 设置附件上传（子）目录  
+        $upload->savePath  =      '/Uploads/say/'; // 设置附件上传（子）目录  
         //var_dump($_FILES);
         // 上传文件   
         $info   =   $upload->upload();
@@ -49,8 +49,8 @@ class UserController extends CommonController {
         $upload = new \Think\Upload();// 实例化上传类  
         $upload->maxSize   =     3145728 ;// 设置附件上传大小  
         //$upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型  
-        $upload->rootPath   =     './Public';
-        $upload->savePath  =      './Uploads/User/'; // 设置附件上传（子）目录  
+        $upload->rootPath   =     './Public/';
+        $upload->savePath  =      '/Uploads/User/'; // 设置附件上传（子）目录  
         //var_dump($_FILES);
         // 上传文件   
         $info   =   $upload->upload();
@@ -70,13 +70,74 @@ class UserController extends CommonController {
         
     }
     
-
+    //显示日记首页
     public function diary(){
 
         $this->display();
 
     }
 
+    //显示写日记
+    public function writeDiary(){
+
+        $this->display();
+
+    }
+
+    //执行写日记提交后的东西
+    public function doDiary(){
+        
+        var_dump($_POST);
+    }
 
 
+    //显示相册
+    public function album(){
+        $this->display();
+    }
+
+    //发布照片或者创建相册页
+    public function publishPhoto(){
+        $this->display();
+    }
+
+
+    // 照片上传
+    public function photoUpload(){
+
+        $upload = new \Think\Upload();// 实例化上传类  
+        $upload->maxSize   =     3145728 ;// 设置附件上传大小  
+        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型  
+        $upload->rootPath   =     './Public';
+        $upload->savePath  =      '/Uploads/photo/'; // 设置附件上传（子）目录  
+        //var_dump($_FILES);
+        // 上传文件   
+        $info   =   $upload->upload();
+
+
+
+        //var_dump($info);  
+        if(!$info) {// 上传错误提示错误信息  
+            // $this->error($upload->getError());  
+            
+            echo 0;
+            exit;
+        }else{// 上传成功 获取上传文件信息  
+            //$this->display('templateList');  
+            foreach($info as $file){
+
+                //生成缩略图
+
+                $image = new \Think\Image();
+
+                $image->open('Public'.$file['savepath'].$file['savename']);
+
+                $image->thumb(170, 170)->save('Public'.$file['savepath'].'thumb_'.$file['savename']);
+
+                echo '/beehive/Public'.$file['savepath'].'thumb_'.$file['savename'];    
+            }
+            
+        }  
+        
+    }
 }
